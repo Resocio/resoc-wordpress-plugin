c502375b92ecf874e060f61a9e89bebf4a6abab5
Philippe Bernard <philippe.bernard@gmail.com>
2018-10-18 17:23:05 +0200

    Old, and work-in-progress, Social by RealFaviconGenerator plugin

---
+++ core/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+const jsSrc = [
+  'assets/js/admin.js',
+  'assets/js/settings.js',
+  'assets/js/vendor.js',
+  'assets/js/bundle.js',
+];
+
+gulp.task('css', function () {
+  return gulp.src('assets/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src(jsSrc)
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(['assets/css/*.scss'], ['css']);
+  gulp.watch(jsSrc, ['scripts']);
+});
+
+gulp.task('default', ['css', 'scripts']);
---
+++ core/includes/lib/class-social-by-realfavicongenerator-facebook-editor.php
@@ -0,0 +1,115 @@
+<?php
+
+class Social_by_RealFaviconGenerator_Facebook_Editor {
+
+	public static function facebook_editor( $post ) {
+
+		$openGraphSerializedData =
+			get_post_meta( $post->ID,
+				Social_by_RealFaviconGenerator::OPTION_OG_SERIALIZED_DATA, true );
+
+		$imageId =
+			get_post_meta( $post->ID,
+				Social_by_RealFaviconGenerator::OPTION_OG_IMAGE_ID, true );
+		if ($imageId) {
+			$imageUrl = wp_get_attachment_url( $imageId );
+		}
+
+		ob_start();
+?>
+	<div class="social-by-rfg-wrap custom-field-panel sbrfg-editor" id="sbrfg-editor">
+		<div>
+			<h3>By <a href="https://realfavicongenerator.net/social" target="_blank">RealFaviconGenerator</a></h3>
+		</div>
+
+		<div class="sbrfg-editor-overall-container">
+      <div class="sbrfg-preview-container">
+				<div class="open-graph-editor-container"></div>
+			</div>
+
+			<div class="sbrfg-fields">
+				<table class="form-table">
+					<tr valign="top">
+						<th scope="row"><label for="sbrfg-title">Title</label></th>
+						<td><input type="text" name="sbrfg-title" placeholder="A title you should change"></td>
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="sbrfg-description">Description</label></th>
+						<td><textarea rows="3" name="sbrfg-description" placeholder="A description you should change, too"></textarea></td>
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="sbrfg-image">Image</label></th>
+						<td>
+              <button class="sbrfg-image-selection-button button-primary">Select Facebook image</button>
+            </td>
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="sbrfg-view">View</label></th>
+						<td>
+              <label for="sbrfg-view-facebook">
+                <input type="radio" name="sbrfg-view" value="facebook" id="sbrfg-view-facebook" checked="checked" />
+                Facebook
+              </label>
+              <label for="sbrfg-view-facebook-caption">
+                <input type="radio" name="sbrfg-view" value="facebook_caption" id="sbrfg-view-facebook-caption" />
+                Facebook - Caption
+              </label>
+              <label for="sbrfg-view-linkedin">
+                <input type="radio" name="sbrfg-view" value="linkedin" id="sbrfg-view-linkedin" />
+                LinkedIn
+              </label>
+              <label for="sbrfg-view-linkedin-caption">
+                <input type="radio" name="sbrfg-view" value="linkedin_caption" id="sbrfg-view-linkedin-caption" />
+                LinkedIn - Caption
+              </label>
+            </td>
+					</tr>
+				</table>
+			</div>
+
+			<div class="sbrfg-clear-fix"></div>
+			<input type="hidden" name="sbrfg-og-serialized-data">
+			<input type="hidden" name="sbrfg-og-image-id">
+		</div>
+	</div>
+
+	<div id="sbrfg-upgrade-notice" style="display:none">
+		Your version of the plugin is outdated.
+		Please <a href="<?php echo get_site_url( null, '/wp-admin/plugins.php' ) ?>" target="_blank">
+			visit your plugins page</a> and update <strong>Social by RealFaviconGenerator</strong>.
+	</div>
+
+	<script>
+  /*
+		jQuery(document).ready(function(e) {
+			var imageId = <?php echo $imageId ? $imageId : 'undefined' ?>;
+			var imageUrl = <?php echo $imageUrl ? '"' . $imageUrl . '"' : 'undefined' ?>;
+			sbrfgInitSocialEditor(
+				jQuery('#sbrfg-editor'),
+				imageId, imageUrl,
+				<?php echo $openGraphSerializedData ? $openGraphSerializedData : 'undefined' ?>,
+				'<?php echo get_site_url() ?>');
+    });
+    */
+  </script>
+
+  <script>
+		jQuery(document).ready(function(e) {
+			var imageId = <?php echo $imageId ? $imageId : 'undefined' ?>;
+      var imageUrl = <?php echo $imageUrl ? '"' . $imageUrl . '"' : 'undefined' ?>;
+      console.log("IMAGE URL=" + imageUrl);
+      var editorContainer = jQuery('#sbrfg-editor');
+
+      sbrfgInitOpenGraphEditor(
+        editorContainer,
+        imageId,
+        imageUrl,
+        '<?php echo get_site_url() ?>'
+      );
+    });
+  </script>
+  
+<?php
+		return ob_get_clean();
+	}
+}
