ada18166e117cffb4cabed3aaf88377fa5bf9e8f
Philippe Bernard <philippe.bernard@gmail.com>
2018-11-05 23:33:54 +0100

    Dead code removed, several small changes such as comments

--- core/includes/lib/class-resoc-social-editor-admin-api.php
+++ core/includes/lib/class-resoc-social-editor-admin-api.php
@@ -107,21 +107,12 @@ class Resoc_Social_Editor_Admin_API {
       Resoc_Social_Editor::OG_TITLE, $title );
     update_post_meta( $post_id,
       Resoc_Social_Editor::OG_DESCRIPTION, $description );
-/*
-    WPSEO_Meta::set_value( 'opengraph-title', $title, $post_id );
-    WPSEO_Meta::set_value( 'opengraph-description', $description, $post_id );
-*/
-    // Image
 
+    // Image
 		$imageSettings = $_POST['rse-og-image-settings'];
 		// See http://stackoverflow.com/questions/2496455/why-are-post-variables-getting-escaped-in-php
     $imageSettings = stripslashes( $imageSettings );
-
-    error_log("DATA=" . $imageSettings);
-
     $imageId = $_POST['rse-og-image-id'];
-    error_log("IMAGE ID " . $imageId);  
-
     $overlay_id = $_POST['rse-og-overlay-image-id'];
 
 		// Check if the data have changed
@@ -160,7 +151,7 @@ class Resoc_Social_Editor_Admin_API {
 		$masterImageUrl = wp_get_attachment_url( $imageId );
 		$masterImageResult = wp_remote_get( $masterImageUrl );
 		if (is_wp_error( $masterImageResult )) {
-			// TODO
+			// BEST: is there a best way to handle the error?
 			error_log( "Cannot download master image: " . $masterImageResult->get_error_message() );
 			return;
 		}
@@ -171,7 +162,7 @@ class Resoc_Social_Editor_Admin_API {
       $overlay_url = wp_get_attachment_url( $overlay_id );
       $overlay_response = wp_remote_get( $overlay_url );
       if (is_wp_error( $overlay_response )) {
-        // TODO
+        // BEST: is there a best way to handle the error?
         error_log( "Cannot download overlay image: " . $overlay_response->get_error_message() );
         return;
       }
@@ -216,7 +207,7 @@ class Resoc_Social_Editor_Admin_API {
     }
     catch(Exception $e) {
       error_log("Error while generating the OpenGraph image: " . $e );
-      // TODO: Process the error
+			// BEST: is there a best way to handle the error?
     }
 
 		return true;
@@ -231,240 +222,6 @@ class Resoc_Social_Editor_Admin_API {
 	 */
 	public function get_picture_url( $post_id ) {
 		return Resoc_Social_Editor::get_files_url( $post_id );
-	}
-
-/*
-	public function store_pictures( $post_id, $rfg_response ) {
-		$working_dir = $this->get_picture_dir( $post_id );
-
-		// Create destination directory if it doesn't exist
-		if ( ! file_exists( $working_dir ) ) {
-			mkdir( $working_dir, 0777, true );
-		}
-
-		// Move pictures to production directory
-		$files = glob( $working_dir . '*' );
-		foreach( $files as $file ) {
-			if ( is_file( $file ) ) {
-			    unlink( $file );
-			}
-    }
-    error_log("PACKAGE DIR IS " . $rfg_response->getProductionPackagePath());
-		$files = glob( $rfg_response->getProductionPackagePath() . '/*' );
-		foreach( $files as $file ) {
-      error_log("MOVE " . $file);
-			if ( is_file( $file ) ) {
-			    rename( $file, $working_dir . basename( $file ) );
-			}
-    }
-    error_log("PICS moved to " . $working_dir);
-    WPSEO_Meta::set_value( 'opengraph-image', $pic, $post_id );
-  }
-*/
-	/**
-	 * Generate HTML for displaying fields
-	 * @param  array   $field Field data
-	 * @param  boolean $echo  Whether to echo the field HTML or return it
-	 * @return void
-	 */
-	public function display_field ( $data = array(), $post = false, $echo = true ) {
-
-		// Get field info
-		if ( isset( $data['field'] ) ) {
-			$field = $data['field'];
-		} else {
-			$field = $data;
-		}
-
-		// Check for prefix on option name
-		$option_name = '';
-		if ( isset( $data['prefix'] ) ) {
-			$option_name = $data['prefix'];
-		}
-
-		// Get saved data
-		$data = '';
-		if ( $post ) {
-
-			// Get saved field data
-			$option_name .= $field['id'];
-			$option = get_post_meta( $post->ID, $field['id'], true );
-
-			// Get data to display in field
-			if ( isset( $option ) ) {
-				$data = $option;
-			}
-
-		} else {
-
-			// Get saved option
-			$option_name .= $field['id'];
-			$option = get_option( $option_name );
-
-			// Get data to display in field
-			if ( isset( $option ) ) {
-				$data = $option;
-			}
-
-		}
-
-		// Show default data if no option saved and default is supplied
-		if ( $data === false && isset( $field['default'] ) ) {
-			$data = $field['default'];
-		} elseif ( $data === false ) {
-			$data = '';
-		}
-
-		$html = '';
-
-		switch( $field['type'] ) {
-
-			case 'text':
-			case 'url':
-			case 'email':
-				$html .= '<input id="' . esc_attr( $field['id'] ) . '" type="text" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $field['placeholder'] ) . '" value="' . esc_attr( $data ) . '" />' . "\n";
-			break;
-
-			case 'password':
-			case 'number':
-			case 'hidden':
-				$min = '';
-				if ( isset( $field['min'] ) ) {
-					$min = ' min="' . esc_attr( $field['min'] ) . '"';
-				}
-
-				$max = '';
-				if ( isset( $field['max'] ) ) {
-					$max = ' max="' . esc_attr( $field['max'] ) . '"';
-				}
-				$html .= '<input id="' . esc_attr( $field['id'] ) . '" type="' . esc_attr( $field['type'] ) . '" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $field['placeholder'] ) . '" value="' . esc_attr( $data ) . '"' . $min . '' . $max . '/>' . "\n";
-			break;
-
-			case 'text_secret':
-				$html .= '<input id="' . esc_attr( $field['id'] ) . '" type="text" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $field['placeholder'] ) . '" value="" />' . "\n";
-			break;
-
-			case 'textarea':
-				$html .= '<textarea id="' . esc_attr( $field['id'] ) . '" rows="5" cols="50" name="' . esc_attr( $option_name ) . '" placeholder="' . esc_attr( $field['placeholder'] ) . '">' . $data . '</textarea><br/>'. "\n";
-			break;
-
-			case 'checkbox':
-				$checked = '';
-				if ( $data && 'on' == $data ) {
-					$checked = 'checked="checked"';
-				}
-				$html .= '<input id="' . esc_attr( $field['id'] ) . '" type="' . esc_attr( $field['type'] ) . '" name="' . esc_attr( $option_name ) . '" ' . $checked . '/>' . "\n";
-			break;
-
-			case 'checkbox_multi':
-				foreach ( $field['options'] as $k => $v ) {
-					$checked = false;
-					if ( in_array( $k, (array) $data ) ) {
-						$checked = true;
-					}
-					$html .= '<label for="' . esc_attr( $field['id'] . '_' . $k ) . '" class="checkbox_multi"><input type="checkbox" ' . checked( $checked, true, false ) . ' name="' . esc_attr( $option_name ) . '[]" value="' . esc_attr( $k ) . '" id="' . esc_attr( $field['id'] . '_' . $k ) . '" /> ' . $v . '</label> ';
-				}
-			break;
-
-			case 'radio':
-				foreach ( $field['options'] as $k => $v ) {
-					$checked = false;
-					if ( $k == $data ) {
-						$checked = true;
-					}
-					$html .= '<label for="' . esc_attr( $field['id'] . '_' . $k ) . '"><input type="radio" ' . checked( $checked, true, false ) . ' name="' . esc_attr( $option_name ) . '" value="' . esc_attr( $k ) . '" id="' . esc_attr( $field['id'] . '_' . $k ) . '" /> ' . $v . '</label> ';
-				}
-			break;
-
-			case 'select':
-				$html .= '<select name="' . esc_attr( $option_name ) . '" id="' . esc_attr( $field['id'] ) . '">';
-				foreach ( $field['options'] as $k => $v ) {
-					$selected = false;
-					if ( $k == $data ) {
-						$selected = true;
-					}
-					$html .= '<option ' . selected( $selected, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
-				}
-				$html .= '</select> ';
-			break;
-
-			case 'select_multi':
-				$html .= '<select name="' . esc_attr( $option_name ) . '[]" id="' . esc_attr( $field['id'] ) . '" multiple="multiple">';
-				foreach ( $field['options'] as $k => $v ) {
-					$selected = false;
-					if ( in_array( $k, (array) $data ) ) {
-						$selected = true;
-					}
-					$html .= '<option ' . selected( $selected, true, false ) . ' value="' . esc_attr( $k ) . '">' . $v . '</option>';
-				}
-				$html .= '</select> ';
-			break;
-
-			case 'image':
-				$image_thumb = '';
-				if ( $data ) {
-					$image_thumb = wp_get_attachment_thumb_url( $data );
-				}
-				$html .= '<img id="' . $option_name . '_preview" class="image_preview" src="' . $image_thumb . '" /><br/>' . "\n";
-				$html .= '<input id="' . $option_name . '_button" type="button" data-uploader_title="' . __( 'Upload an image' , 'resoc-social-editor' ) . '" data-uploader_button_text="' . __( 'Use image' , 'resoc-social-editor' ) . '" class="image_upload_button button" value="'. __( 'Upload new image' , 'resoc-social-editor' ) . '" />' . "\n";
-				$html .= '<input id="' . $option_name . '_delete" type="button" class="image_delete_button button" value="'. __( 'Remove image' , 'resoc-social-editor' ) . '" />' . "\n";
-				$html .= '<input id="' . $option_name . '" class="image_data_field" type="hidden" name="' . $option_name . '" value="' . $data . '"/><br/>' . "\n";
-			break;
-
-			case 'color':
-				?><div class="color-picker" style="position:relative;">
-			        <input type="text" name="<?php esc_attr_e( $option_name ); ?>" class="color" value="<?php esc_attr_e( $data ); ?>" />
-			        <div style="position:absolute;background:#FFF;z-index:99;border-radius:100%;" class="colorpicker"></div>
-			    </div>
-			    <?php
-			break;
-
-		}
-
-		switch( $field['type'] ) {
-
-			case 'checkbox_multi':
-			case 'radio':
-			case 'select_multi':
-				$html .= '<br/><span class="description">' . $field['description'] . '</span>';
-			break;
-
-			default:
-				if ( ! $post ) {
-					$html .= '<label for="' . esc_attr( $field['id'] ) . '">' . "\n";
-				}
-
-				$html .= '<span class="description">' . $field['description'] . '</span>' . "\n";
-
-				if ( ! $post ) {
-					$html .= '</label>' . "\n";
-				}
-			break;
-		}
-
-		if ( ! $echo ) {
-			return $html;
-		}
-
-		echo $html;
-
-	}
-
-	/**
-	 * Validate form field
-	 * @param  string $data Submitted value
-	 * @param  string $type Type of field to validate
-	 * @return string       Validated value
-	 */
-	public function validate_field ( $data = '', $type = 'text' ) {
-
-		switch( $type ) {
-			case 'text': $data = esc_attr( $data ); break;
-			case 'url': $data = esc_url( $data ); break;
-			case 'email': $data = is_email( $data ); break;
-		}
-
-		return $data;
 	}
 
 	/**
@@ -500,21 +257,6 @@ class Resoc_Social_Editor_Admin_API {
 	}
 
 	/**
-	 * Dispay field in metabox
-	 * @param  array  $field Field data
-	 * @param  object $post  Post object
-	 * @return void
-	 */
-	public function display_meta_box_field ( $field = array(), $post ) {
-
-		if ( ! is_array( $field ) || 0 == count( $field ) ) return;
-
-		$field = '<p class="form-field"><label for="' . $field['id'] . '">' . $field['label'] . '</label>' . $this->display_field( $field, $post, false ) . '</p>' . "\n";
-
-		echo $field;
-	}
-
-	/**
 	 * Save metabox fields
 	 * @param  integer $post_id Post ID
 	 * @return void
@@ -525,22 +267,6 @@ class Resoc_Social_Editor_Admin_API {
 
 		$this->add_meta_box('rse-meta-facebook', 'Share on Facebook and LinkedIn',
 			get_post_types( array( 'public' => true ) ) );
-
-		if ( ! $post_id ) return;
-
-		$post_type = get_post_type( $post_id );
-
-		$fields = apply_filters( $post_type . '_custom_fields', array(), $post_type );
-
-		if ( ! is_array( $fields ) || 0 == count( $fields ) ) return;
-
-		foreach ( $fields as $field ) {
-			if ( isset( $_REQUEST[ $field['id'] ] ) ) {
-				update_post_meta( $post_id, $field['id'], $this->validate_field( $_REQUEST[ $field['id'] ], $field['type'] ) );
-			} else {
-				update_post_meta( $post_id, $field['id'], '' );
-			}
-		}
 	}
 
   public function settings_menu() {
