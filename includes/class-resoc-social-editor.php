5d5ca06cbbce6208ad275634fec8917fde5f5f2d
Philippe Bernard <philippe.bernard@gmail.com>
2018-12-12 13:16:13 +0100

    Components updated: Snap to border for easier image edition

--- includes/class-resoc-social-editor.php
+++ includes/class-resoc-social-editor.php
@@ -174,7 +174,7 @@ class Resoc_Social_Editor {
 	 * @return  void
 	 */
 	public function admin_enqueue_scripts ( $hook = '' ) {
-    $resoc_components_version = '0.0.5';
+    $resoc_components_version = '0.0.7';
 
 		wp_enqueue_script( $this->_token . '-react',
 			'https://unpkg.com/react@16/umd/react.development.js',
